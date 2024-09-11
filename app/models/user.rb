dd4777234d2943cd9a9b9c2993ec27a938238092
Cristobal Dominguez <cristobald@gmail.com>
2024-09-11 12:29:59 +0200

    Adds Option for User Avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :rememberable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
+
+  has_one_attached :avatar
 end
