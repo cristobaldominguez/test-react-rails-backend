7c5d03947df96800670df72e89b98e71b962674b
Cristobal Dominguez <cristobald@gmail.com>
2024-09-11 12:37:52 +0200

    Adds Avatar to Registration/Login Response

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 
   has_one_attached :avatar
+
+  def avatar_url
+    if self.avatar&.attachment
+      if Rails.env.development?
+        Rails.application.routes.url_helpers.rails_blob_url(self.avatar, only_path: true)
+      else
+        self.avatar_url = self.avatar&.service_url&.split("?")&.first
+      end
+    end
+  end
 end
